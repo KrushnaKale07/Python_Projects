8e8465648893348ee59684e9e536869da0c9cde7
KrushnaKale07 <krushnakale0007@gmail.com>
2023-11-23 22:11:00 +0530

    study prog on individual DT

---
+++ IndividualDT.py
@@ -0,0 +1,20 @@
+# Individual Datatype
+# int
+# a = 12
+# print(a)          // 12
+# print(type(a))    //Int
+# print()
+
+# float
+# a = 12.33
+# print(a)          //!2.33
+# print(type(a))    //float
+# print()
+
+# bool
+# a = True
+# b = False
+# print(a)          //True
+# print(b)          //False
+# print(type(a))    //bool
+# print(type(b))    //bool
