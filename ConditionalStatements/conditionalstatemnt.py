3709a7f5aa9330c6d26eff8a9378291f8a0572e4
KrushnaKale07 <krushnakale0007@gmail.com>
2023-11-25 23:38:01 +0530

    Prog on conditional statement

---
+++ ConditionalStatements/conditionalstatemnt.py
@@ -0,0 +1,23 @@
+# if statement
+
+# a = 10
+# if a>0:
+#     print(f'{a} is positive number')
+
+# elif statement
+
+# a=13
+# if a%2==0:
+#         print(f'{a} is divisible by 2')
+# elif a%5==0:
+#         print(f'{a} is divisible by 5')
+# else:
+#         print(f'{a} is not divisible by 2 and 5')
+
+# else statement
+a=3
+if a%2==0:
+        print(f'{a} is divisible by 2')
+else:
+        print(f'{a} is not divisible by 2')
+
