6217fa626dfc63a8c8a855f98eb3a4dd2449a915
KrushnaKale07 <krushnakale0007@gmail.com>
2023-12-02 18:29:26 +0530

    Programs on enumerate function

---
+++ Looping_Statement/Enumerate.py
@@ -0,0 +1,28 @@
+# String
+# s = 'hello'
+#
+# for index, value in enumerate(s):
+#     print(index, value)
+
+
+# d={'Sport':'Cricket', 'institute':'Qspider', 'Country':'India', 'Nation_bird':'Peacock', 'Subject':'Python', 'Brand':'Apple'}
+#
+# d1={}
+#
+# for index, value in enumerate(d):
+#     d1[value] = [d.get(value), index]
+#
+# print(d1)
+
+
+# Program to print file name with extension .pdf
+#
+# files = ['Youtube.txt', 'amazon.pdf', 'facebook.pdf']
+#
+# for index in range (0, len(files)):
+#
+#     element = files[index]
+#     list = element.split('.')
+#
+#     if list[-1] == 'pdf':
+#         print(element)
